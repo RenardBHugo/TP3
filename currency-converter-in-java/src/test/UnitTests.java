09dc262e05521d5c5de62c4bcc4fd8a71b5e08ef
RenardBHugo <38992367+RenardBHugo@users.noreply.github.com>
2019-12-13 00:57:01 -0500

    TestC et D

--- currency-converter-in-java/src/test/UnitTests.java
+++ currency-converter-in-java/src/test/UnitTests.java
@@ -8,6 +8,10 @@ import currencyConverter.Currency;
 import currencyConverter.MainWindow;
 
 class UnitTests {
+	//array de currency
+	private ArrayList<Currency> currencies = Currency.init();
+	//array de currency vide
+	private ArrayList<Currency> currencyVide = new ArrayList<Currency>();
 	@Test
 	void CurrencyTestBlackBox() {
 		//assertEquals(0.0, Currency.convert(-1.0, 3.291));
@@ -20,7 +24,6 @@ class UnitTests {
 	void mainWindowTestBlackBox() {
 		double epsilon = 0.1;
 		double amount  = 100000.0;
-		ArrayList<Currency> currencies = Currency.init();
 		
 		// Test toutes les valeurs d'échange entre elles (ex: USD -> EUR =?= EUR <- USD)
 		for (int i = 0; i < currencies.size(); i++) {
@@ -47,5 +50,29 @@ class UnitTests {
 		assertEquals(0.0, MainWindow.convert(Currency.init().get(2).getName(), "Test", Currency.init(), 10.0));
 		assertEquals(15.2, MainWindow.convert(Currency.init().get(2).getName(), Currency.init().get(3).getName(), Currency.init(), 10.0));
 	}
+	void mainWindowTestC() {
 
+
+		ArrayList<Currency> currencyNomVide = new ArrayList<Currency>();
+		currencyNomVide.add( new Currency("US Dollar", null) );
+
+		//cas 1 : Currency 2 et currency 1 premiere monnaie du tableau(donc egales), shortNameCurrency 2 != null
+		assertEquals(1.0,MainWindow.convert("US Dollar", "US Dollar", currencies, 1.0));
+		//cas 2 : Currency 2 et currency 1 premiere monnaie du tableau(donc egales), shortNameCurrency 2 == null
+		assertEquals(0.0,MainWindow.convert("US Dollar", "US Dollar", currencyNomVide, 1.0));
+		//cas 3-4 : currencies.size() == 0
+		assertEquals(0.0,MainWindow.convert("US Dollar", "US Dollar", currencyVide, 1.0));
+		//cas 5-6 impossibles a tester.
+	}
+	@Test
+	void mainWindowTestD() {
+		//cas 1 : tout est vrai
+		assertEquals(1.0,MainWindow.convert("US Dollar", "US Dollar", currencies, 1.0));
+		//cas 2: devise 2 n'existe pas
+		assertEquals(0.0,MainWindow.convert("US Dollar", "Boing", currencies, 1.0));
+		//cas 3: devise 3 1 n'existe pas
+		assertEquals(0.0,MainWindow.convert("Boing", "US Dollar", currencies, 1.0));
+		//cas 4 : currencies.size() == 0
+		assertEquals(0.0,MainWindow.convert("US Dollar", "US Dollar", currencyVide, 1.0));
+	}
 }
